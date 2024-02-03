585aa24fb29bfdc7cf5f009f5d310ee217a0dd9f
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-03 09:16:07 -0800

    WIP: refactor champ funcs

--- include/champion.h
+++ include/champion.h
@@ -3,10 +3,10 @@
 #include "op.h"
 
 // function prototypes
-champion_t *init_champion(flag_t *flags);                   // initializes an empty champion
-int create_champion(flag_t *flags, char *filename);         // creates champion from file
-header_t *get_header(int fd);                               // returns header of champion
-op_t *get_instructions(champion_t *champ, int fd);             // returns array of instructions
-void free_champion(champion_t *champ);                      // frees champion
+champion_t *init_champion(flag_t *flags);                       // initializes an empty champion
+champion_t *create_champion(flag_t *flags, char *filename);     // creates champion from file
+header_t *get_header(int fd);                                   // returns header of champion
+op_t *get_instructions(champion_t *champ, int fd);              // returns array of instructions
+void free_champion(champion_t *champ);                          // frees champion
 
 #endif
