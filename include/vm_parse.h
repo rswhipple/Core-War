8237b6923c56efc3964b4e8f6d2f4d7ea0bd3987
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-03 07:13:37 -0800

    chore: create flag_t struct

--- include/vm_parse.h
+++ include/vm_parse.h
@@ -2,8 +2,16 @@
 #define _VM_PARSE_H_
 #include "op.h"
 
+typedef struct flag_s {
+    int num_champions;
+    int dump;
+    int id;
+    int address;
+} flag_t;
+
 // function prototypes
 int parse_args(int argc, char **argv);
+flag_t init_flag();
 void print_usage();
 
 #endif
