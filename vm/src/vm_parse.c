e818e29aefa22475f391ccb4124345246a9bd1a5
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-06 11:18:08 -0800

    updates

--- vm/src/vm_parse.c
+++ vm/src/vm_parse.c
@@ -42,6 +42,10 @@ champion_t *parse_args(int argc, char **argv, flag_t** flags)
     int i = 1;
     champion_t *head = NULL;
 
+<<<<<<< HEAD:vm/src/vm_parse.c
+=======
+
+>>>>>>> 4f2d565 (updates):src/vm_parse.c
     // read flags 
     while (i < argc ) {
         if (argv[i][0] == '-') {
@@ -64,6 +68,7 @@ champion_t *parse_args(int argc, char **argv, flag_t** flags)
                 // TODO add error handling if champion isn't created
             if (head == NULL) {
                 head = create_champion(*flags, argv[i]);
+<<<<<<< HEAD:vm/src/vm_parse.c
             } else {
                 champion_t *temp = head; 
                 while (temp->next) {
@@ -71,6 +76,10 @@ champion_t *parse_args(int argc, char **argv, flag_t** flags)
                 }
                 temp->next = create_champion(*flags, argv[i]);
             }
+=======
+            }
+            create_champion(*flags, argv[i]);
+>>>>>>> 4f2d565 (updates):src/vm_parse.c
             (*flags)->id = 0;
             (*flags)->address = 0;
             (*flags)->num_champions += 1;
