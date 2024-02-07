2af73e80d748ed54c1dd61c3dfe82d1b2abc4185
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-02-07 00:14:58 -0800

    Add failing test

--- tests/sample_test.c
+++ tests/sample_test.c
@@ -15,10 +15,17 @@ static void compare_integer_failure(void **state) {
   assert_int_not_equal(i, 5);
 }
 
+static void compare_fail(void **state) {
+  int i = 4;
+
+  assert_int_equal(i, 5);
+}
+
 int main(void) {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test(compare_integer_pass),
     cmocka_unit_test(compare_integer_failure),
+    cmocka_unit_test(compare_fail),
   };
   return cmocka_run_group_tests_name("compare integer tests", tests, NULL, NULL);
 }
