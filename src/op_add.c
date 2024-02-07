5d8db773b3fbe7682d3ac9bc8172c9586ce2d179
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-02-06 22:50:41 -0800

    Add update_carry and inst_add functions

--- src/op_add.c
+++ src/op_add.c
@@ -1,5 +1,14 @@
 #include "../include/op.h"
 
-int inst_add(champion_t *champ, core_t *core, code_t code, int *inst) {
+// Adds the value stored in the first register to the second register's value and
+// stores the result to the third register.
+int inst_add(champion_t *champ, core_t *core, code_t code, int *args) {
+  int operand_1_reg = args[VALUE_1];
+  int operand_2_reg = args[VALUE_2];
+  int dest_reg = args[VALUE_3];
+
+  champ->reg[dest_reg] = champ->reg[operand_1_reg] + champ->reg[operand_2_reg];
+  update_carry(champ, dest_reg);
+
   return 0;
 }
