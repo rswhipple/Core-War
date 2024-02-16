311cae43b1724e5b3f0048565ee0dd661e8e836c
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-16 15:32:00 -0800

    CHORE: add structs t_header and t_array

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -4,5 +4,27 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+
+#define PROG_NAME_LENGTH 128
+#define COMMENT_LENGTH 2048
+#define COREWAR_EXEC_MAGIC 0xea83f3
+
+// structs
+typedef struct s_header {
+  int magic;
+  char prog_name[PROG_NAME_LENGTH + 1];
+  int prog_size;
+  char comment[COMMENT_LENGTH + 1];
+} t_header;
+
+typedef struct s_string_array {
+    int size;
+    char **array;
+} t_array;
+
+// function prototypes
+int write_header(int fd, t_header* header);
+t_header *init_header(void);
 
 #endif
