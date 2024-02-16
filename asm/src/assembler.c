5bfdad47f1c6d39d419cb52bb77808f3e0d1b622
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-16 15:33:27 -0800

    CHORE: add funcs to create and write header

--- asm/src/assembler.c
+++ asm/src/assembler.c
@@ -1,4 +1,25 @@
 #include "../include/asm.h"
 
-// convert instruction to machine code
+// initialize header
+t_header *init_header(void) {
+    t_header *header = malloc(sizeof(t_header));
+    header->magic = COREWAR_EXEC_MAGIC;
+    memset(header->prog_name, 0, PROG_NAME_LENGTH); // TODO change to my_memset
+    memset(header->comment, 0, COMMENT_LENGTH);     // TODO change to my_memset
+    return header;
+}
 
+// convert instruction to machine code
+int write_header(int fd, t_header* header) 
+{
+    // Header
+    ssize_t bytes_written = write(fd, header, sizeof(t_header));
+
+    // ERROR + EXIT
+    if (bytes_written != 500) {  // TODO figure out size
+        return EXIT_FAILURE;
+    }
+    free(header);
+
+    return EXIT_SUCCESS;
+}
