880d89b00ae8790fa018caa28825a6489bf4f8e1
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-16 16:25:14 -0800

    CHORE: cleanup

--- asm/include/memory.h
+++ asm/include/memory.h
@@ -6,7 +6,7 @@
 t_header *init_header(void);
 t_array *init_t_array(void);
 char *init_str(int size);
-void free_t_array(t_array *arr);
-void free_header(t_header *header);
+// void free_t_array(t_array *arr);
+// void free_header(t_header *header);
 
 #endif
