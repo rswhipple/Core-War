76d123a29d247d5348e7a094b7eb0c36f436fc17
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-18 16:19:00 -0800

    CHORE: add replace_ext()

--- asm/include/asm_parse.h
+++ asm/include/asm_parse.h
@@ -5,6 +5,7 @@
 // function prototypes
 int execute_asm(char *filename);
 FILE *create_cor_file(char *filename);
+char *replace_ext(char *filename);
 int read_file(FILE *fp, t_header **header, t_array **inst) ;
 void print_usage();
 
