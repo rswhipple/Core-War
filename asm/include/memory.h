8a9af92935253cb0300dad2066f9ba5bc6dc1661
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-18 16:20:33 -0800

    CHORE: refactor init_t_array()

--- asm/include/memory.h
+++ asm/include/memory.h
@@ -4,7 +4,7 @@
 
 // function prototypes
 t_header *init_header(void);
-t_array *init_t_array(void);
+t_array *init_t_array(int size);
 char *init_str(int size);
 // void free_t_array(t_array *arr);
 // void free_header(t_header *header);
--- asm/src/memory.c
+++ asm/src/memory.c
@@ -1,19 +1,23 @@
 #include "../include/memory.h"
+#include "../include/helper.h"
 
 // initialize header struct
 t_header *init_header(void) {
     t_header *header = malloc(sizeof(t_header));
-    header->magic = COREWAR_EXEC_MAGIC;
-    memset(header->prog_name, 0, PROG_NAME_LENGTH); // TODO change to my_memset
-    memset(header->comment, 0, COMMENT_LENGTH);     // TODO change to my_memset
+    header->magic = 15369203;
+    my_memset(header->prog_name, 0, PROG_NAME_LENGTH + 2); 
+    header->prog_size = 0;
+    my_memset(header->comment, 0, COMMENT_LENGTH + 2);     
     return header;
 }
 
 // initialize string array struct
-t_array *init_t_array(void) {
+t_array *init_t_array(int size) {
     t_array *new = malloc(sizeof(t_array));
+    new->array = malloc(sizeof(char*) * size);
     new->size = 0;
-    new->array = NULL;
+
+    for (int i = 0; i < size; i++) { new->array[i] = NULL; }
 
     return new;
 }
