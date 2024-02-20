4b0a73b0010bdc582d10b931aa96205dc9c34b25
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-20 11:55:15 -0800

    CHORE: refactor remove_line_title() to generalize

--- asm/include/asm_parse.h
+++ asm/include/asm_parse.h
@@ -7,7 +7,7 @@ int execute_asm(char *filename);
 t_node *read_file(FILE *fp, t_header **header);
 FILE *create_cor_file(char *filename);
 char *replace_ext(char *filename);
-void remove_line_title(t_header **header, char *line, int size);
+void remove_line_title(char *dest, char *line, int size);
 t_node *string_to_node(char *src);
 void token_to_arg(t_node **args, char *tok, int type);
 
