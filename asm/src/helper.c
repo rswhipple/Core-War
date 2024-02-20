cdcd8214e4544a4dc4f59463957eb715cb38fd19
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-20 15:47:53 -0800

    CHORE: add my_strcmp()

--- asm/src/helper.c
+++ asm/src/helper.c
@@ -87,6 +87,29 @@ char* my_strchr(char* str_1, char char_1)
     return NULL;
 }
 
+int my_strcmp(char* str_1, char* str_2)
+{
+    int i;
+    int str_to_int_1 = 0;
+    int str_to_int_2 = 0;
+
+    for (i = 0; str_1[i] != '\0'; i++) {
+        str_to_int_1 += str_1[i];
+    } 
+
+    for (i = 0; str_2[i] != '\0'; i++) {
+        str_to_int_2 += str_2[i];
+    } 
+    
+    if (str_to_int_1 == str_to_int_2) {
+        return 0;
+    } else if (str_to_int_1 > str_to_int_2) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
 int my_strncmp(char* str_1, char* str_2, int index)
 {
     int i = 0;
