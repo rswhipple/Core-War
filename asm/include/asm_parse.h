306a59004883c07a825d3ffed76da648c5f1b63e
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-20 15:47:31 -0800

    CHORE: remove labels when parsing

--- asm/include/asm_parse.h
+++ asm/include/asm_parse.h
@@ -10,5 +10,6 @@ char *replace_ext(char *filename);
 void remove_line_title(char *dest, char *line, int size);
 t_node *string_to_node(char *src);
 void token_to_arg(t_node **args, char *tok, int type);
+void ttoa_remove_char(t_node **args, char *tok, int type);
 
 #endif
