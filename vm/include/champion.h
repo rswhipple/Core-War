44083669119c6d0006f76d09a4650ac9ae55c31c
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-28 11:49:42 -0800

    CHORE: add_print_header

--- vm/include/champion.h
+++ vm/include/champion.h
@@ -7,6 +7,7 @@
 champion_t *init_champion(flag_t *flags);                       // initializes an empty champion
 champion_t *create_champion(flag_t *flags, char *filename);     // calls init_champion and read_file
 int read_file(champion_t **champ, int fd);                      // parses .cor file into champion
+void print_header(header_t *header);                            // tests whether header content was parsed correctly
 void free_champion(champion_t *champ);                          // frees champion
 void print_champions(champion_t *head);                         // print champion contents
 #endif
