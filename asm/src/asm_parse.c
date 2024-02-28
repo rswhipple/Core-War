fd6cd09fdf0175fab47d924fc64266aca2ea882c
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-28 07:49:51 -0800

    CHORE: add arg to read_file

--- asm/src/asm_parse.c
+++ asm/src/asm_parse.c
@@ -10,19 +10,20 @@ int execute_asm(char *filename) {
     // create header and instruction line storage
     t_header *header = init_header();
     t_node *inst_head = NULL;
+    int total_num_inst = 0;
 
     // open, read and parse .S file
     const char *read = "r";
     FILE *fp = fopen(filename, read);
     if (!fp) return EXIT_FAILURE;
-    inst_head = read_file(fp, &header);
+    inst_head = read_file(fp, &header, &total_num_inst);
     fclose(fp);
 
     // write header
     if (write_header(cor, header)) return EXIT_FAILURE;
 
     // write instructions
-    if (write_inst(cor, inst_head)) return EXIT_FAILURE;
+    if (write_inst(cor, inst_head, total_num_inst)) return EXIT_FAILURE;
 
     // cleanup
     free_nodes(inst_head);
@@ -32,7 +33,7 @@ int execute_asm(char *filename) {
     return EXIT_SUCCESS;
 }
 
-t_node *read_file(FILE *fp, t_header **header) {
+t_node *read_file(FILE *fp, t_header **header, int *total) {
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
@@ -49,14 +50,17 @@ t_node *read_file(FILE *fp, t_header **header) {
             continue;
         } 
         else {
+            (*total)++;
             if (line[nread - 1] == '\n') line[nread - 1] = '\0';
 
             if (!head) {
                 head = string_to_node(line);
+                head->id = (*total);
             } else {
                 t_node *tmp = head;
                 while (tmp->next) tmp = tmp->next;
                 tmp->next = string_to_node(line);
+                tmp->next->id = (*total);
             }
         }
     }
@@ -100,8 +104,8 @@ t_node *string_to_node(char *src) {
     // check for label
     int tok_len = my_strlen(tokens->array[0]);
     if (tokens->array[0][tok_len - 1] == LABEL_CHAR) {
-        args->label = init_str(tok_len + 1);
-        my_strcpy(args->label, tokens->array[0]);
+        args->label = tokens->array[0];
+        args->label[tok_len - 1] = '\0';
         i++;
     }
 
@@ -117,17 +121,17 @@ t_node *string_to_node(char *src) {
         else if (tokens->array[i][0] == 'r') {
             ttoa_remove_char(&args, tokens->array[i], 1);
             args->num_bytes++;
-            args->count++;
+            args->param_count++;
         }
         else if (tokens->array[i][0] == DIRECT_CHAR) {
             ttoa_remove_char(&args, tokens->array[i], 2);
             args->num_bytes += 4;
-            args->count++;
+            args->param_count++;
         }
         else {
             token_to_arg(&args, tokens->array[i], 3);
             args->num_bytes += 2;
-            args->count++;
+            args->param_count++;
         }
         i++;
     }
@@ -140,22 +144,22 @@ t_node *string_to_node(char *src) {
 
 void token_to_arg(t_node **args, char *tok, int type) 
 {
-    (*args)->array[(*args)->count] = init_arg();
-    t_arg *tmp = (*args)->array[(*args)->count];
+    (*args)->array[(*args)->param_count] = init_arg();
+    t_arg *tmp = (*args)->array[(*args)->param_count];
     tmp->arg = init_str(my_strlen(tok) + 1);
     my_strcpy(tmp->arg, tok);
     tmp->type = type;
-    printf("(*args)->array[%i]->arg = |%s|, type is %i\n", (*args)->count, tmp->arg, tmp->type);  // TESTING
+    printf("(*args)->array[%i]->arg = |%s|, type is %i\n", (*args)->param_count, tmp->arg, tmp->type);  // TESTING
 
 }
 
 void ttoa_remove_char(t_node **args, char *tok, int type) 
 {
-    (*args)->array[(*args)->count] = init_arg();
-    t_arg *tmp = (*args)->array[(*args)->count];
+    (*args)->array[(*args)->param_count] = init_arg();
+    t_arg *tmp = (*args)->array[(*args)->param_count];
     tmp->arg = init_str(my_strlen(tok));
     my_strcpy(tmp->arg, tok + 1);
     tmp->type = type;
-    printf("(*args)->array[%i]->arg = |%s|, type is %i\n", (*args)->count, tmp->arg, tmp->type);  // TESTING
+    printf("(*args)->array[%i]->arg = |%s|, type is %i\n", (*args)->param_count, tmp->arg, tmp->type);  // TESTING
 
 }
