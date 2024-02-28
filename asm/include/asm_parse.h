140ee33e9a6463cae8f1301346899ac263bbeaed
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-28 12:31:18 -0800

    CHORE: add args to functs

--- asm/include/asm_parse.h
+++ asm/include/asm_parse.h
@@ -4,11 +4,11 @@
 
 // function prototypes
 int execute_asm(char *filename);
-t_node *read_file(FILE *fp, t_header **header, int *total);
+t_node *read_file(FILE *fp, t_header **header, t_prog_size *size); 
 FILE *create_cor_file(char *filename);
 char *replace_ext(char *filename);
 void remove_line_title(char *dest, char *line, int size);
-t_node *string_to_node(char *src);
+t_node *string_to_node(char *src, t_prog_size *size);
 void token_to_arg(t_node **args, char *tok, int type);
 void ttoa_remove_char(t_node **args, char *tok, int type);
 
