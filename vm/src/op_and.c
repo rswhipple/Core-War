751b13b1985d9cddfd073e409be11a14f1fa34e2
Nikita Gaidamachenko <123207709+ngaidamachenko@users.noreply.github.com>
2024-03-01 13:16:05 -0500

    Adding runtime check to op_and.c

--- vm/src/op_and.c
+++ vm/src/op_and.c
@@ -4,7 +4,11 @@ int inst_and(champion_t *champ, core_t *core, code_t code, int *args) {
   int operand_1_regs = args[VALUE_1];
   int operand_2_regs = args[VALUE_2];
   int dest_reg = args[VALUE_3];
-
+  
+  if (operand_1_reg == 0 || operand_2_reg == 0 || dest_reg == 0) {
+    return 1;
+  }
+  
   champ->reg[dest_reg] = champ->reg[operand_1_regs] & champ->reg[operand_2_regs];
   update_carry(champ, dest_reg);
 
