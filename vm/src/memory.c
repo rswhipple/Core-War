ac342ec0364f5c6fb36b6826b7d093e4b3357608
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-02-29 22:45:18 -0800

    Revise free_champions

--- vm/src/memory.c
+++ vm/src/memory.c
@@ -1,24 +1,21 @@
- #include "../include/op.h"
-#include "../include/memory.h"
+#include "op.h"
+#include "memory.h"
 
-
-// free everything
+// free champion and core
 void cleanup(champion_t *head, core_t *core) {
-    free_champions(head);
-    free(core);
+  free_champions(head);
+  free(core);
 }
 
 // free champions
 void free_champions(champion_t *head) {
-    champion_t *curr = head;
-    champion_t *next = head->next; 
-    while (curr) {
-        free(curr->champ_header);
-        free(curr);
-        curr = next;
-        if (next) {
-            next = next->next;
-        }
-    }
+  champion_t *curr = head;
+  champion_t *next = curr;
+  while (curr) {
+    curr = curr->next;
+    free(next->champ_header);
+    free(next);
+    next = curr;
+  }
 }
 
