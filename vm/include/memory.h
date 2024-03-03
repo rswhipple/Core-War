73f0d8d618ea826ee28ccf01914f13c0876550e3
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-03 10:36:29 -0800

    CHORE: add memory handling for new data types

--- vm/include/memory.h
+++ vm/include/memory.h
@@ -4,8 +4,11 @@
 #include "vm_parse.h"
 
 // function prototypes
-void cleanup(champion_t *head, core_t *core);   // frees all data
-void free_champions(champion_t *champ);         // frees champions
+void cleanup(champion_t *head, core_t *core);       // frees all data
+void free_champions(champion_t *champ);             // frees champions
+inst_t init_inst(void);
+inst_array_t *init_inst_array(int size);
+void free_inst_array(inst_array_t *array);
 // void free_core(core_t *core);                   // frees core
 
 #endif
