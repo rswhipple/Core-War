3ffe3b12446e6ef101d83109df0496c28c6bdd75
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-03 10:36:46 -0800

    CHORE: add memory handling for new data types

--- vm/src/memory.c
+++ vm/src/memory.c
@@ -22,3 +22,29 @@ void free_champions(champion_t *head) {
     }
 }
 
+inst_t init_inst(void) {
+    inst_t holder = {
+        .opcode = 0,
+        .param_desc = 0,
+        .value_1 = 0,
+        .value_2 = 0,
+        .value_3 = 0,
+    };
+
+    return holder;
+}
+
+inst_array_t *init_inst_array(int size) {
+    inst_array_t *new = malloc(sizeof(inst_array_t));
+    new->array = malloc(sizeof(inst_t*) * size);
+    new->size = size;
+
+    for (int i = 0; i < size; i++) new->array[i] = NULL;
+
+    return new;
+}
+
+void free_inst_array(inst_array_t *inst) {
+    for (int i = 0; i < inst->size; i++) free(inst->array[i]);
+    free(inst);
+}
