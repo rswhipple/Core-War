da7646add212865f0bb6bde0cfff1f4dd3a6583d
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-04 09:20:10 -0800

    CHORE: refactor inst init funcs

--- vm/include/memory.h
+++ vm/include/memory.h
@@ -6,9 +6,8 @@
 // function prototypes
 void cleanup(champion_t *head, core_t *core);       // frees all data
 void free_champions(champion_t *champ);             // frees champions
-inst_t init_inst(void);
-inst_array_t *init_inst_array(int size);
-void free_inst_array(inst_array_t *array);
+inst_t *init_inst(void);
+inst_t **init_inst_array(int size);
 // void free_core(core_t *core);                   // frees core
 
 #endif
