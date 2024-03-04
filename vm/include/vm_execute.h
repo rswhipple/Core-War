6f3e9ff32604d76b041d5eedd7a98e5ac67eea12
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-04 09:21:25 -0800

    WIP: start game loop

---
+++ vm/include/vm_execute.h
@@ -0,0 +1,9 @@
+#ifndef _VM_EXECUTE_H_
+#define _VM_EXECUTE_H_
+#include "op.h"
+
+// function prototypes
+int game_loop(core_t *core);
+void print_winner(core_t *core);
+
+#endif
--- vm/src/vm_execute.c
+++ vm/src/vm_execute.c
@@ -1,7 +1,12 @@
 /*
     vm_execute needs to include
 
-        1. a scheduler function
-        
+        1. a scheduler function (game_loop)
+
         2. print winner function
 */
+#include "../include/op.h"
+
+int game_loop(core_t *core) {
+    return EXIT_SUCCESS;
+}
