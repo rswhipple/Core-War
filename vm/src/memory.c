15311f55a3d162dfb5041d1aa2d6c88cbfb6f893
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-05 09:28:13 -0800

    WIP: refactor to add cursor_t

--- vm/src/memory.c
+++ vm/src/memory.c
@@ -13,7 +13,9 @@ void free_champions(champion_t *head) {
     champion_t *curr = head;
     champion_t *next = head->next; 
     while (curr) {
-        free(curr->champ_header);
+        free(curr->cursor);
+        free(curr->name);
+        free(curr->comment);
         free(curr);
         curr = next;
         if (next) {
@@ -22,26 +24,45 @@ void free_champions(champion_t *head) {
     }
 }
 
-inst_t *init_inst(void) {
-    inst_t *new = malloc(sizeof(inst_t));
-    new->opcode = 0;
-    new->param_desc = 0;
-    new->value_1 = 0;
-    new->value_2 = 0;
-    new->value_3 = 0;
 
-    return new;
+char* init_str(int size)  
+{
+    char* str = malloc(sizeof(char) * size + 1);
+
+    for (int i = 0; i < size + 1; i++) { str[i] = '\0'; }
+
+    return str;
 }
 
-inst_t **init_inst_array(int size) {
-    inst_t **new = malloc(sizeof(inst_t*) * size);
+char *init_and_strncpy(char *src) {
+    int len = my_strlen(src);
+    char *str = init_str(len);
+    my_strcpy(str, src);
 
-    for (int i = 0; i < size; i++) new[i] = init_inst();
-
-    return new;
+    return str;
 }
 
-void free_inst_array(inst_t **inst, int size) {
-    for (int i = 0; i < size; i++) free(inst[i]);
-    free(inst);
-}
+
+// inst_t *init_inst(void) {
+//     inst_t *new = malloc(sizeof(inst_t));
+//     new->opcode = 0;
+//     new->param_desc = 0;
+//     new->value_1 = 0;
+//     new->value_2 = 0;
+//     new->value_3 = 0;
+
+//     return new;
+// }
+
+// inst_t **init_inst_array(int size) {
+//     inst_t **new = malloc(sizeof(inst_t*) * size);
+
+//     for (int i = 0; i < size; i++) new[i] = init_inst();
+
+//     return new;
+// }
+
+// void free_inst_array(inst_t **inst, int size) {
+//     for (int i = 0; i < size; i++) free(inst[i]);
+//     free(inst);
+// }
