559140e7b811ea6e719694b024cf5480e7fe962d
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-05 09:25:18 -0800

    CHORE: move init_str() and refactor

--- vm/include/memory.h
+++ vm/include/memory.h
@@ -1,13 +1,16 @@
 #ifndef _MEMORY_H_
 #define _MEMORY_H_
 #include "op.h"
-#include "vm_parse.h"
+// #include "vm_parse.h"
+#include "helper.h"
 
 // function prototypes
 void cleanup(champion_t *head, core_t *core);       // frees all data
 void free_champions(champion_t *champ);             // frees champions
-inst_t *init_inst(void);
-inst_t **init_inst_array(int size);
+char *init_str(int size);
+char *init_and_strncpy(char *src);
+// inst_t *init_inst(void);
+// inst_t **init_inst_array(int size);
 // void free_core(core_t *core);                   // frees core
 
 #endif
