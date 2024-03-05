c8abb68ecf5a3ad37d3bf7e24775219904a3f473
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-05 14:17:48 -0800

    CHORE: add helper funcs

--- vm/include/helper.h
+++ vm/include/helper.h
@@ -9,5 +9,7 @@ char* my_strncpy(char* dst, char* src, int n);
 char* my_strcpy(char* dst, char* src);
 char* my_strcat(char *dst, char *src);
 int my_strncmp(char* str_1, char* str_2, int index);
+void my_putstr(char *str);
+void my_puterror(char *str);
 
 #endif
