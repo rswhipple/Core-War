ecc1a7ebdf9d0daf11ca0c571ef16ed03299dc45
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-05 09:26:07 -0800

    CHORE: create print_tests.h

---
+++ vm/include/print_tests.h
@@ -0,0 +1,9 @@
+#ifndef _PRINT_TESTS_H_
+#define _PRINT_TESTS_H_
+#include "op.h"
+
+// function prototypes
+void print_header(header_t *header);
+void print_inst_buf(header_t *header, u_int8_t *buf, size_t size);
+void print_champions(champion_t *head);
+#endif
