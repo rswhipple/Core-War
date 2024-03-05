f86bb8c732e0bf7a730a3bd66da7e90e597051e3
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-05 09:24:48 -0800

    CHORE: add funcs and refactor

--- vm/include/champion.h
+++ vm/include/champion.h
@@ -3,15 +3,13 @@
 #include "op.h"
 #include "vm_parse.h"
 
-#define BUF_SIZE 512
-
 // function prototypes
-champion_t *init_champion(flag_t *flags);                       // initializes an empty champion
-champion_t *create_champion(flag_t *flags, char *filename);     // calls init_champion and read_file
+champion_t *create_champ(flag_t *flags, char *filename);
+champion_t *init_champion(flag_t *flags);                    
+cursor_t *init_cursor(flag_t *flags);
 int read_file(champion_t **champ, int fd);                      // parses .cor file into champion
-int convert_to_inst(u_int8_t *buf, size_t prog_size);
+int convert_to_inst(char *buf, size_t prog_size);
 void print_header(header_t *header);                            // tests whether header content was parsed correctly
-void print_inst_buf(header_t *header, u_int8_t *buf, size_t size);
-void free_champion(champion_t *champ);                          // frees champion
+void print_inst_buf(header_t *header, char *buf, size_t size);
 void print_champions(champion_t *head);                         // print champion contents
 #endif
