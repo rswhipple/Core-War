5ef4da73a3030df5e53ba0678b55fb25f86568f8
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-05 13:46:10 -0800

    CHORE: refactor

--- vm/src/vm_parse.c
+++ vm/src/vm_parse.c
@@ -20,8 +20,9 @@ champion_t *parse_args(int argc, char **argv, flag_t** flags)
     champion_t *head = NULL;
 
     // read flags 
-    // TODO: the option handlier is still having segv issues, fix in the following lines
+    
     while (i < argc ) {
+        // TODO: the option handlier is still having segv issues, TEST AND DEBUG!!
         if (argv[i][0] == '-') {
             if (argv[i][1] == 'n') {
                 // set id
@@ -39,7 +40,7 @@ champion_t *parse_args(int argc, char **argv, flag_t** flags)
             i += 2;     // increment i by 2
         } else {
             // create champion, adjust flags
-                // TODO: add error handling if champion isn't created
+            // TODO: add error handling if champion isn't created
             if (head == NULL) {
                 head = create_champ(*flags, argv[i]);
             } else {
@@ -55,13 +56,18 @@ champion_t *parse_args(int argc, char **argv, flag_t** flags)
         }
         i++;
     }
+
     print_champions(head);
     return head;    // return head of champion linked list
 }
 
 void print_usage()
 {
-    // print usage instructions
+    char *message = "USAGE\n\t./corewar [-dump nbr_cycle] [[-n prog_number] [-a load_address] [prog_name] ...\nDESCRIPTION\n"
+                    "\t-dump nbr_cycle\n\tdumps the memory after the nbr_cycle execution (if the round isn’t already over) with the following format: 32 bytes/line in hexadecimal (A0BCDEFE1DD3...)\n"
+                    "\t-n prog_number\n\tsets the next program’s number. By default, the first free number in the parameter order\n"
+                    "\t-a load_address\n\tsets the next program’s loading address. When no address is specified, optimize the addresses so that the processes are as far away from each other as possible. The addresses are MEM_SIZE modulo";
+    my_putstr(message);
 }
 
 
