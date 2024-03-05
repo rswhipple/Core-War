3792a1d421ef53ee527500a16ce3324b67f8a90b
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-05 13:47:37 -0800

    CHORE: refactor init_core and load_champs

--- vm/src/core_s.c
+++ vm/src/core_s.c
@@ -1,6 +1,7 @@
 #include "../include/op.h"
 #include "../include/core.h"
 #include "../include/memory.h"
+#include "../include/print_tests.h"
 
 // initialize core
 core_t *init_core(champion_t *head, flag_t *flags) {
@@ -9,8 +10,8 @@ core_t *init_core(champion_t *head, flag_t *flags) {
     if (core == NULL) {
         return NULL;
     }
-    memset(core->memory, 0, MEM_SIZE);  // initialize memory to 0
-    core->champions = NULL;
+    memset(core->memory, 0, MEM_SIZE);
+    core->champions = head;
     core->num_champions = flags->num_champions;
     core->cycle_to_die = CYCLE_TO_DIE;
     core->cycle_delta = CYCLE_DELTA;
@@ -18,13 +19,34 @@ core_t *init_core(champion_t *head, flag_t *flags) {
     core->dump = flags->dump;
     core->cycle = 0;
 
-    core->load_champion = load_champion;
+    load_champions(core, head);
+
+
+    // TODO figure out op_tab
+
+    // TODO make champion_t list and cursor_t list circular
 
     free(flags);    // all information from flag_t has been recorded
 
     return core;
 }
 
-void load_champion(core_t *core, champion_t *champ) {
+void load_champions(core_t *core, champion_t *head) {
+    int offset = MEM_SIZE / core->num_champions;
+
+    champion_t *tmp = head;
+    int i = 1;
+    while (tmp) {
+        tmp->cursor->index_start = offset * (i - 1);
+        if (tmp->next) {
+            tmp->cursor->next = tmp->next->cursor;
+        }
+        else {
+            tmp->cursor->next = head->cursor;
+        }
+        i++;
+        tmp = tmp->next;
+    }
 
 }
+
