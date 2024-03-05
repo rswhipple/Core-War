1e4f21da2fe8af96e00e5c32ed1483087858d065
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-05 12:17:11 -0800

    CHORE: cleanup

--- vm/include/champion.h
+++ vm/include/champion.h
@@ -7,9 +7,6 @@
 champion_t *create_champ(flag_t *flags, char *filename);
 champion_t *init_champion(flag_t *flags);                    
 cursor_t *init_cursor(flag_t *flags);
-int read_file(champion_t **champ, int fd);                      // parses .cor file into champion
-int convert_to_inst(char *buf, size_t prog_size);
-void print_header(header_t *header);                            // tests whether header content was parsed correctly
-void print_inst_buf(header_t *header, char *buf, size_t size);
-void print_champions(champion_t *head);                         // print champion contents
+int read_file(champion_t **champ, int fd);  // parses .cor file into champion
+
 #endif
