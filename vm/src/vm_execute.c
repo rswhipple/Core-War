57312f977a6d889150e593fe3290de05433c022d
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-06 09:35:53 -0800

    WIP: game_loop

--- vm/src/vm_execute.c
+++ vm/src/vm_execute.c
@@ -8,5 +8,22 @@
 #include "../include/op.h"
 
 int game_loop(core_t *core) {
+    // bool no_winner;
+    // cursor_t *cursor;
+
+    // no_winner = true;
+    // cursor = core->cursors;
+    // while (no_winner) {
+    //     if (cursor->flag) check_for_winner(core->champions, &no_winner);
+    //     if (!cursor->dead) {
+    //         execute_inst();
+    //     }
+    //     cursor = cursor->next;
+    // }
+
+    // print_winner();
+
     return EXIT_SUCCESS;
 }
+
+// void print_winner() {}
