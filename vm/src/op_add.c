7b939e468703a2225ceae0550204450cd6d5c1fa
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-06 14:15:59 -0800

    WIP updates for inst execution

--- vm/src/op_add.c
+++ vm/src/op_add.c
@@ -2,13 +2,13 @@
 
 // Adds the value stored in the first register to the second register's value and
 // stores the result to the third register.
-int inst_add(champion_t *champ, core_t *core, code_t code, int *args) {
-  int operand_1_reg = args[VALUE_1];
-  int operand_2_reg = args[VALUE_2];
-  int dest_reg = args[VALUE_3];
+int inst_add(core_t *core, cursor_t* cursor) {
+  int r1 = (int)core->memory[cursor->ac++];
+  int r2 = (int)core->memory[cursor->ac++];
+  int r3 = (int)core->memory[cursor->ac++];
 
-  champ->reg[dest_reg] = champ->reg[operand_1_reg] + champ->reg[operand_2_reg];
-  update_carry(champ, dest_reg);
+  cursor->reg[r3] = cursor->reg[r1] + cursor->reg[r2];
+  update_carry(cursor->parent, r3);
 
   return 0;
 }
