8f913f5cffe06ab4f883350567309d76d51a9afc
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-06 15:48:02 -0800

    CHORE reoder prototypes

--- vm/include/vm_execute.h
+++ vm/include/vm_execute.h
@@ -4,9 +4,10 @@
 
 // function prototypes
 int game_loop(core_t *core);
-void print_winner(core_t *core);
 void check_for_winner(core_t **core, cursor_t **cursor, bool *no_winner);
+void print_winner(core_t *core);
 int execute_inst(core_t *core, cursor_t *cursor);
 int	command_to_opcode(core_t *core, cursor_t *cursor);
+
 
 #endif
