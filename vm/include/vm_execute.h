e0e315bbdf26476a5106cfaf99f9598d5d5cc1ad
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-06 09:46:48 -0800

    CHORE add func prototypes

--- vm/include/vm_execute.h
+++ vm/include/vm_execute.h
@@ -5,5 +5,7 @@
 // function prototypes
 int game_loop(core_t *core);
 void print_winner(core_t *core);
+bool check_for_winner(champion_t *head, bool no_winner);
+int execute_inst(core_t *core, cursor_t *cursor);
 
 #endif
