a621bdee817d2652afddfc43adce68b7c00b90f1
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-06 16:09:01 -0800

    CHORE add logs

--- vm/src/vm_execute.c
+++ vm/src/vm_execute.c
@@ -1,10 +1,3 @@
-/*
-    vm_execute needs to include
-
-        1. a scheduler function (game_loop)
-
-        2. print winner function
-*/
 #include "../include/vm_execute.h"
 
 
@@ -14,12 +7,15 @@ int game_loop(core_t *core) {
 
     no_winner = true;
     cursor = core->cursors;
+    printf("~~~###/// THE CORE WARS COMMENCE ///###~~~\n");
     while (no_winner) {
         if (cursor->flag) check_for_winner(&core, &cursor, &no_winner);
         if (!cursor->dead) {
+            printf("champion %s #%i\n", cursor->parent->name, cursor->parent->id);
             execute_inst(core, cursor);
         }
         cursor = cursor->next;
+        no_winner = false;
     }
 
     print_winner(core);
@@ -33,6 +29,15 @@ void check_for_winner(core_t **core, cursor_t **cursor, bool *no_winner) {
     if ((*core)->live_champs == 1) {
         *no_winner = false;
     }
+}
+
+void print_winner(core_t *core) {
+    printf("--------------------------------------------\n");
+    printf("\n\tA winner has been declared\n\n\n");
+    printf("\nThe winner of the war over the cores is...\n\n\n");
+    printf("\n\t%s\n\n", core->champions->name);
+    printf("--------------------------------------------\n\n");
+    printf("~~~###/// THE CORE WARS ARE OVER ///###~~~\n\n");
 }
 
 
@@ -54,11 +59,13 @@ int	command_to_opcode(core_t *core, cursor_t *cursor) {
     i = (unsigned char)core->memory[cursor->ac]; 
     if (i >= 0x01 && i <= 0x0F) {
         opcode = ((int)i) - 1;
+        printf("opcode = %i\n", opcode);
         return opcode;
     }
     else
         return 0;
 }
+
 
 // void	ft_add_cycle(t_env *e, t_cursor *cursor)
 // {
@@ -70,4 +77,4 @@ int	command_to_opcode(core_t *core, cursor_t *cursor) {
 // 	else
 // 		ft_update_cursor(e, cursor, 1);
 // }
-void print_winner(core_t *core) {}
+
