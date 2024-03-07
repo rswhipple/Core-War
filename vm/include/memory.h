b2fc0f22be7537c5065e66e2f7e9641772f6098b
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-07 10:51:45 -0800

    CHORE cleanup

--- vm/include/memory.h
+++ vm/include/memory.h
@@ -5,12 +5,9 @@
 #include "helper.h"
 
 // function prototypes
-void cleanup(core_t *core);       // frees all data
+void cleanup(core_t *core);                         // frees all data
 void free_champions(champion_t *champ);             // frees champions
 char *init_str(int size);
 char *init_and_strncpy(char *src);
-// inst_t *init_inst(void);
-// inst_t **init_inst_array(int size);
-// void free_core(core_t *core);                   // frees core
 
 #endif
--- vm/src/memory.c
+++ vm/src/memory.c
@@ -42,27 +42,3 @@ char *init_and_strncpy(char *src) {
     return str;
 }
 
-
-// inst_t *init_inst(void) {
-//     inst_t *new = malloc(sizeof(inst_t));
-//     new->opcode = 0;
-//     new->param_desc = 0;
-//     new->value_1 = 0;
-//     new->value_2 = 0;
-//     new->value_3 = 0;
-
-//     return new;
-// }
-
-// inst_t **init_inst_array(int size) {
-//     inst_t **new = malloc(sizeof(inst_t*) * size);
-
-//     for (int i = 0; i < size; i++) new[i] = init_inst();
-
-//     return new;
-// }
-
-// void free_inst_array(inst_t **inst, int size) {
-//     for (int i = 0; i < size; i++) free(inst[i]);
-//     free(inst);
-// }
