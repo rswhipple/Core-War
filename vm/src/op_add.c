8db631dc98cab242a7e76fde9a6b1027ffed0ed2
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-07 10:48:59 -0800

    CHORE add testing log

--- vm/src/op_add.c
+++ vm/src/op_add.c
@@ -9,8 +9,9 @@ int inst_add(core_t *core, cursor_t* cursor) {
 
   int r1 = (int)core->memory[cursor->ac + 2];
   int r2 = (int)core->memory[cursor->ac + 3];
-  int r3 = (int)core->memory[cursor->ac + 4];
+  int r3 = get_reg(core, cursor, 3);
 
+  printf("Testing get_reg in inst_add:\n\tr1 = %i\nr2 = %i\nr3 = %i\n", r1, r2, r3);
   cursor->reg[r3] = cursor->reg[r1] + cursor->reg[r2];
   update_carry(cursor->parent, r3);
   update_cursor(core, cursor, 5);
