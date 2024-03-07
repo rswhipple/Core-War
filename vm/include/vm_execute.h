dd8ca0d90d3af43616ab30cb2fb23398a46c1023
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-06 16:51:31 -0800

    refactor

--- vm/include/vm_execute.h
+++ vm/include/vm_execute.h
@@ -7,7 +7,7 @@ int game_loop(core_t *core);
 void check_for_winner(core_t **core, cursor_t **cursor, bool *no_winner);
 void print_winner(core_t *core);
 int execute_inst(core_t *core, cursor_t *cursor);
+void add_cycle(core_t **core);
 int	command_to_opcode(core_t *core, cursor_t *cursor);
-
 
 #endif
