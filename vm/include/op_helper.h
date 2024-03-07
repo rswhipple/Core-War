cc1fa817d5e6894ddfaa337f1354e770a42a3ad6
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-07 10:47:06 -0800

    CHORE add needed funcs

--- vm/include/op_helper.h
+++ vm/include/op_helper.h
@@ -4,6 +4,14 @@
 
 // function prototypes
 void update_cursor(core_t *core, cursor_t *cursor, int i);
+void update_cycles(core_t **core);
 void update_carry(cursor_t *cursor, int dest_reg);
+int get_reg(core_t *core, cursor_t *cursor, int i);
+int get_dir(core_t *core, cursor_t *cursor, int i);
+int get_label(core_t *core, cursor_t *cursor, int i);
+int	get_ind(core_t *core, cursor_t *cursor, int i);
+void is_alive(core_t *core);
+void print_stats(core_t *core);
+void add_cycle(cursor_t *cursor);
 
 #endif
