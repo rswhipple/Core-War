637835191d90d7d9ea6d7d1515619d55862da889
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-07 10:47:33 -0800

    WIP add needed funcs, update_cycles

--- vm/src/op_helper.c
+++ vm/src/op_helper.c
@@ -11,6 +11,20 @@ void update_cursor(core_t *core, cursor_t *cursor, int i) {
 	}
 }
 
+void update_cycles(core_t **core) {
+	(*core)->cycle_count += 1;
+	(*core)->total_cycles += 1;
+	if ((*core)->cycle_count == (*core)->cycle_to_die) {
+		(*core)->cycle_count = 0;
+		(*core)->live_count += 1;
+		if ((*core)->live_count == (*core)->nbr_live) {
+			(*core)->live_count = 0;
+		}
+		is_alive(*core);
+		print_stats(*core);
+	}
+}
+
 
 // update the champion's carry flag if the previous operation returns 0
 void update_carry(cursor_t *cursor, int dest_reg) {
@@ -19,15 +33,19 @@ void update_carry(cursor_t *cursor, int dest_reg) {
   }
 }
 
+int get_reg(core_t *core, cursor_t *cursor, int i) {
+	return (int)core->memory[cursor->ac + i];
+}
+
 
 int get_dir(core_t *core, cursor_t *cursor, int i)
 {
 	unsigned int result;
 
 	result = (MASK_FF(core->memory[MOD(cursor->ac + i)]) << 24) |
-            	(MASK_FF(core->memory[MOD(cursor->ac + i + 1)]) << 16) |
-                	(MASK_FF(core->memory[MOD(cursor->ac + i + 2)]) << 8) |
-                    	MASK_FF(core->memory[MOD(cursor->ac + i + 3)]);
+				(MASK_FF(core->memory[MOD(cursor->ac + i + 1)]) << 16) |
+				(MASK_FF(core->memory[MOD(cursor->ac + i + 2)]) << 8) |
+				MASK_FF(core->memory[MOD(cursor->ac + i + 3)]);
 
 	return (int)result;
 }
@@ -38,8 +56,8 @@ int get_label(core_t *core, cursor_t *cursor, int i)
 
 	result = (0x00 << 24) |
             	(MASK_FF(core->memory[MOD(cursor->ac + i + 1)]) << 16) |
-                	(MASK_FF(core->memory[MOD(cursor->ac + i + 2)]) << 8) |
-                    	MASK_FF(core->memory[MOD(cursor->ac + i + 3)]);
+                (MASK_FF(core->memory[MOD(cursor->ac + i + 2)]) << 8) |
+                MASK_FF(core->memory[MOD(cursor->ac + i + 3)]);
 
 	return (int)result;
 }
@@ -53,3 +71,9 @@ int	get_ind(core_t *core, cursor_t *cursor, int i)
 
 	return (int)result;
 }
+
+void is_alive(core_t *core) {}
+
+void print_stats(core_t *core) {}
+
+void add_cycle(cursor_t *cursor) {}
