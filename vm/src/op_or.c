3aef544d23fe72e4853575fb478111c22bfdfa75
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-07 20:09:45 -0800

    CHORE debug

--- vm/src/op_or.c
+++ vm/src/op_or.c
@@ -9,7 +9,7 @@ int inst_or(core_t *core, cursor_t* cursor) {
 
   printf("Testing get_reg in inst_or:\n\tr1 = %i\nr2 = %i\nr3 = %i\n", r1, r2, r3);
   cursor->reg[r3] = cursor->reg[r1] | cursor->reg[r2];
-  update_carry(cursor->parent, r3);
+  update_carry(cursor, r3);
   update_cursor(core, cursor, 5);
 
   return 0;
--- vm/src/op_zjmp.c
+++ vm/src/op_zjmp.c
@@ -7,9 +7,8 @@ Zjmp %23 -> if carry == 1, Counter + 23 % IDX_MOD to Counter */
 int inst_zjmp(core_t *core, cursor_t* cursor) {
   if (cursor->carry == 1) {
     // jump to that index
-    int dir = get_dir(core, cursor, 1);
-    int new_index = cursor->ac + MOD(dir);
-    update_cursor(core, cursor, new_index);
+    int index = MOD(get_dir(core, cursor, 1));
+    update_cursor(core, cursor, index);
     return EXIT_SUCCESS;
   }
 
