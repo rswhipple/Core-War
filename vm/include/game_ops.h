05816731878545f89256e1e28aeb69991ccb14b8
Rebecca W Silverstein <rswhipple@gmail.com>
2024-03-07 17:32:47 -0800

    WIP stdout for game end

--- vm/include/game_ops.h
+++ vm/include/game_ops.h
@@ -11,11 +11,14 @@ int get_dir(core_t *core, cursor_t *cursor, int i);
 int get_label(core_t *core, cursor_t *cursor, int i);
 int	get_ind(core_t *core, cursor_t *cursor, int i);
 void add_cycle(int opcode, cursor_t *cursor);
+void add_run_cycle(cursor_t *cursor);
 int is_alive(core_t *core);
-void game_over(core_t *core);
+void announce_death(champion_t *champion);
+void game_over(core_t *core, bool dump_flag);
 void calc_tie(core_t *core);
 void print_stats(core_t *core);
 void print_winner(champion_t *core);
 void print_tie(champion_t *core);
+void print_draw(champion_t *champion);
 
 #endif
